1832e5534e56db3ad2c5dde2029e16e9c113c184
GMKrishna99 <gmohankrishna23@gmail.com>
2023-06-16 10:17:26 +0530

    chore : auth modal hook for

---
+++ hooks/useAuthModel.ts
@@ -0,0 +1,16 @@
+import { on } from "events";
+import { create } from "zustand";
+
+interface AuthModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useAuthModal = create<AuthModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useAuthModal;
