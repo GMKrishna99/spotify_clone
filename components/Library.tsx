0fdea3c4687e8a3ffb46ff7a7b06bc632b73b80c
GMKrishna99 <gmohankrishna23@gmail.com>
2023-06-16 18:39:27 +0530

    feat : user songs upload feature

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,21 @@
 'use client'
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from 'react-icons/ai'
+import useAuthModal from "@/hooks/useAuthModel";
+import { useUser } from "@/hooks/useUser";
+import useUploadModel from "@/hooks/useUploadModel";
 const Library = () => {
+
+    const authModal = useAuthModal()
+    const { user, subscription } = useUser()
+    const uploadModel = useUploadModel()
     const onClick = () => {
-        // handle upload
+        if (!user) {
+            return authModal.onOpen()
+        }
+        // check for subscriptions
+      
+        return uploadModel.onOpen()
     }
     return (
         <div className="flex flex-col ">
@@ -14,7 +26,7 @@ const Library = () => {
                         Your library
                     </p>
                 </div>
-                <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" />
+                <AiOutlinePlus size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" onClick={onClick} />
             </div>
             <div className="flex flex-col gap-y-2 mt-4 px-3">
                 List of Songs
